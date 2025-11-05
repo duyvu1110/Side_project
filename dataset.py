5112c3dac2901ccafe7a068fbcd5a2be1b09a594
duyvu1110 <100179213+duyvu1110@users.noreply.github.com>
2025-11-05 14:52:54 +0700

    check dataloader

--- dataset.py
+++ dataset.py
@@ -91,7 +91,7 @@ class ZaloAIDataset(Dataset):
         sampling_rate = total_video_frames / self.num_frames
         sampled_idxs = [int(round(sampling_rate * i)) for i in range(self.num_frames)]
         # Ensure indices are within bounds
-        sampled_idxs = [min(max(0, i), total_video_frames - 1) for i in sampled_idxs]
+        sampled_idxs = [102,103,104,105,106]
         
         # --- 4. Load Video Frames ---
         video_pil_frames = load_video_frames_at_indices(video_path, sampled_idxs)
